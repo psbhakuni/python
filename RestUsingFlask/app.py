267c524db0a0492e8ff184b5dd829a8315a7abc7
Prakash Bhakuni <psbhakuni@gmail.com>
2021-03-06 11:13:19 +0530

    REST Application Using Flask

---
+++ RestUsingFlask/app.py
@@ -0,0 +1,57 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask#installing-python-and-flask
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Books Store Home PAge</h1>
+<p>Now you can test other operations on books.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "id field not found in reqeust. Please specify the book's id."
+    result = []
+
+    for book in books:
+        if book['id'] == id:
+            result.append(book)
+
+    # Use jsonify to transform list of dictionaries to the JSON format.
+    return jsonify(result)
+
+
+app.run()
